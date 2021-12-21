8482ef3df72ccecacfd9d0e84becf805e503cad2
necroprogrammer <Morbid6dead@gmail.com>
2021-12-21 22:09:29 +0300

    second commit

--- parser/views.py
+++ parser/views.py
@@ -4,9 +4,9 @@ from time import sleep
 from os import chdir, system
 
 def shutdown():
-    chdir(r'/home/necrodeather/Desktop/django/parser/parsers/')
+    chdir(r'/home/necrodeather/Desktop/Wyrestorm-django/parser/parsers/')
     system('ps a | grep app.py | cat > pid.txt')
-    with open('/home/necrodeather/Desktop/django/parser/parsers/pid.txt', 'r') as pid:
+    with open('/home/necrodeather/Desktop/Wyrestorm-django/parser/parsers/pid.txt', 'r') as pid:
         lines = pid.readlines()
         system(f"kill -9 {lines[0].strip()[:5]}")
     pid.close()
@@ -15,16 +15,19 @@ def index(request):
     return render(request, 'parser/index.html')
 
 def power_button(request):
-    chdir(r'/home/necrodeather/Desktop/django/parser/parsers/')
+    print('power')
+    chdir(r'/home/necrodeather/Desktop/Wyrestorm-django/parser/parsers/')
     system(r'python3 app.py')
     return HttpResponseRedirect('/admin')
 
 def restart_button(request):
+    print('restart')
     shutdown()
     sleep(5)
     system(r'python3 app.py') 
     return HttpResponseRedirect('/admin')
 
 def shutdown_button(request):
+    print('shutdown')
     shutdown()
     return HttpResponseRedirect('/admin')
